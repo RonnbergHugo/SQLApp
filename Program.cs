b0e63b07707b55be8f30669f6f2995cddcbd99ce
Hugo <hugo45044@gmail.com>
2026-02-11 15:46:51 +0100

    Added menu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,59 @@
-﻿namespace SQLapp
+﻿using Spectre.Console;
+using System.Drawing;
+
+namespace SQLapp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AnsiConsole.Clear();
+            AnsiConsole.Write(new FigletText("School App").Centered().Color(Spectre.Console.Color.Green));
+
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[yellow]Välj ett alternativ[/]")
+                    .AddChoices(new[]
+                    {
+                        "List Students",
+                        "List Courses",
+                        "Enroll Student",
+                        "Create Submission",
+                        "Update Grade",
+                        "Delete Student",
+                        "Reports",
+                        "Exit"
+                    }));
+
+            switch (choice)
+            {
+                case "List Students":
+                    break;
+
+                case "List Courses":
+                    break;
+
+                case "Enroll Student":
+                    break;
+
+                case "Create Submission":
+                    break;
+
+                case "Update Grade":
+                    break;
+
+                case "Delete Student":
+                    break;
+
+                case "Reports":
+                    break;
+
+                case "Exit":
+                    return;
+            }
+
+            AnsiConsole.MarkupLine("\n[grey]Tryck valfri tangent...[/]");
+            Console.ReadKey();
         }
     }
 }
