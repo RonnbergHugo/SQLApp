a4ae9687b0aa02b367ca8488b154e2960cf51b86
RonnbergHugo <229610585+RonnbergHugo@users.noreply.github.com>
2026-02-12 11:55:21 +0100

    Update joins.sql

--- queries/joins.sql
+++ queries/joins.sql
@@ -27,3 +27,23 @@ FROM orders o
 JOIN orderItems oi ON o.id = oi.orderId
 JOIN products p ON oi.productId = p.id
 GROUP BY o.id;
+
+-- 4. Top customers
+SELECT 
+    c.id,
+    c.name,
+    COUNT(o.id) AS orderCount
+FROM customers c
+LEFT JOIN orders o ON c.id = o.customerId
+GROUP BY c.id, c.name
+ORDER BY orderCount DESC;
+
+-- 5. Top selling products
+SELECT 
+    p.id,
+    p.name,
+    SUM(oi.quant) AS TotalSold
+FROM products p
+JOIN orderItems oi ON p.id = oi.productId
+GROUP BY p.id, p.name
+ORDER BY TotalSold DESC;
